4902fe2aac6b974fbff6719fb769a34c60c45fa9
gr8kev <ebukakels@gmail.com>
2023-09-01 21:54:26 +0100

    criptonx

--- criptonx.js
+++ criptonx.js
@@ -1,25 +1,25 @@
 const header = document.querySelector("header");
-window.addEventListener("scroll",function(){
-    header.classList.toggle("sticky" ,window.scrollY > 50)
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 50)
 })
-const observer = new   IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        if(entry.isIntersecting){
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
             entry.target.classList.add("show-items");
-        }else{
+        } else {
             entry.target.classList.remove("show-items")
         }
     })
 })
 
 const scrollScale = document.querySelectorAll(".scroll-scale");
-scrollScale.forEach((el)=>observer.observe(el));
+scrollScale.forEach((el) => observer.observe(el));
 
 const scrollBottom = document.querySelectorAll(".scroll-bottom");
-scrollBottom.forEach((el)=>observer.observe(el));
+scrollBottom.forEach((el) => observer.observe(el));
 
 const scrollTop = document.querySelectorAll(".scroll-top");
-scrollTop.forEach((el)=>observer.observe(el));
+scrollTop.forEach((el) => observer.observe(el));
 
 const bar = document.getElementById('bar')
 const nav = document.getElementById('navbar')
@@ -27,14 +27,14 @@ const close = document.getElementById('close')
 
 
 if (bar) {
-  bar.addEventListener('click', () => {
-    nav.classList.add('activ')
+    bar.addEventListener('click', () => {
+        nav.classList.add('activ')
 
-})
+    })
 }
 if (close) {
-  close.addEventListener('click', () => {
-    nav.classList.remove('activ')
+    close.addEventListener('click', () => {
+        nav.classList.remove('activ')
 
-})
+    })
 }
